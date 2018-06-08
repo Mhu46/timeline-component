900a73c0368f44c50ad208da94c8c8ae2e1337f0
Faisal Ali <faisal.siddiqui@capillarytech.com>
2018-06-08 22:13:27 +0530

    updated model of the data

--- src/app/components/horizontal-timeline/timeline-element.ts
+++ src/app/components/horizontal-timeline/timeline-element.ts
@@ -1,7 +1,40 @@
-export interface TimelineElement {
-  caption: string;
-  date: Date;
-  title: string;
+export class TimelineElement {
+  id: string;
+  name: string;
+  severity: string;
+  start: string;
+  end: string;
+  causes: any;
   selected?: boolean;
-  content: string;
+
+  constructor(element) {
+    this.id = element.id;
+    this.name = element.name;
+    this.severity = element.severity;
+    this.start = element.start;
+    this.end = element.end;
+    element.causes.forEach(subElement => {
+      const se = new TimeLineSubElement(subElement);
+      this.causes.push(se);
+    });
+  }
+}
+
+export class TimeLineSubElement {
+  id: string;
+  text: string;
+  severity: string;
+  start: string;
+  end: string;
+  affected: any;
+
+  constructor(subElement) {
+    this.id = subElement.id;
+    this.text = subElement.text;
+    this.severity = subElement.severity;
+    this.start = subElement.start;
+    this.end = subElement.end;
+    this.affected = subElement.affected;
+  }
+
 }
