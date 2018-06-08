cb7989bd323c76d761c0f6056a6d24db8dd824ec
Faisal Ali <faisal.siddiqui@capillarytech.com>
2018-06-08 15:37:03 +0530

    added timline component and sidenav with navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HorizontalTimelineComponent } from './components/horizontal-timeline/horizontal-timeline.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HorizontalTimelineComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
